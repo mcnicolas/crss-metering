1f4cc7244376b511482fc245a353aba0dd9b7abe
Julius Cessar Lapugot <jlapugot@exist.com>
2016-10-10 13:50:22 +0800

    Add missing EnableDiscoveryClient annotation

--- metering-app/src/main/java/com/pemc/crss/metering/Application.java
+++ metering-app/src/main/java/com/pemc/crss/metering/Application.java
@@ -2,10 +2,12 @@ package com.pemc.crss.metering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
