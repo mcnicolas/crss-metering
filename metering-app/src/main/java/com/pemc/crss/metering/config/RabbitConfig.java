e1adbc415ce8c7102ec3c3aed8acd484d85b4be9
Julius Cessar Lapugot <jlapugot@exist.com>
2016-10-25 12:41:03 +0800

    Add security config
     - add dependencies for security oauth
     - move rabbit config to config package

--- metering-app/src/main/java/com/pemc/crss/metering/config/RabbitConfig.java
+++ metering-app/src/main/java/com/pemc/crss/metering/config/RabbitConfig.java
@@ -1,4 +1,4 @@
-package com.pemc.crss.metering;
+package com.pemc.crss.metering.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
---
+++ metering-app/src/main/java/com/pemc/crss/metering/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.pemc.crss.metering.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@Configuration
+@EnableOAuth2Client
+@EnableResourceServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
