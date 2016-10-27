91097fb39d99df17dd5ee811317933cfcb6d9475
Chris Lim <clim@exist.com>
2016-10-27 15:17:07 +0800

    Added missing file.

---
+++ metering-upload/src/main/java/com/pemc/crss/meter/upload/LoginException.java
@@ -0,0 +1,9 @@
+package com.pemc.crss.meter.upload;
+
+public class LoginException extends Exception {
+
+    public LoginException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
