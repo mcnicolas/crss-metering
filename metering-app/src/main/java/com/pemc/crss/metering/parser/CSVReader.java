46c9793e80a9ccb0f8a980203d68ed2ce58dbe0c
Chris Lim <clim@exist.com>
2016-11-04 10:57:03 +0800

    [137786] Refactor CSV parsing.

--- metering-app/src/main/java/com/pemc/crss/metering/parser/CSVReader.java
+++ metering-app/src/main/java/com/pemc/crss/metering/parser/CSVReader.java
@@ -2,113 +2,77 @@ package com.pemc.crss.metering.parser;
 
 import com.pemc.crss.metering.dto.MeterData2;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.supercsv.cellprocessor.ParseDouble;
-import org.supercsv.cellprocessor.Trim;
-import org.supercsv.cellprocessor.ift.CellProcessor;
-import org.supercsv.exception.SuperCsvReflectionException;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.supercsv.io.CsvListReader;
 import org.supercsv.io.ICsvListReader;
-import org.supercsv.util.CsvContext;
-import org.supercsv.util.MethodCache;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Method;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import static org.supercsv.prefs.CsvPreference.EXCEL_PREFERENCE;
+import static org.supercsv.prefs.CsvPreference.STANDARD_PREFERENCE;
 
 @Slf4j
 public class CSVReader implements MeterQuantityReader {
 
-    private final MethodCache cache = new MethodCache();
+    private DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm");
 
     @Override
     public List<MeterData2> readData(InputStream inputStream) throws IOException {
         List<MeterData2> meterData = new ArrayList<>();
 
-        try (ICsvListReader reader = new CsvListReader(new InputStreamReader(inputStream), EXCEL_PREFERENCE)) {
-
+        try (ICsvListReader reader = new CsvListReader(new InputStreamReader(inputStream), STANDARD_PREFERENCE)) {
             reader.getHeader(true);
-            String[] headers = getHeaders();
-            CellProcessor[] processors = getProcessors();
 
             List<String> row;
             while ((row = reader.read()) != null) {
-                meterData.add(populateBean(headers, processors, reader.getRowNumber(), reader.getLineNumber(), row));
+                meterData.add(populateBean(row));
             }
         }
 
         return meterData;
     }
 
-    private MeterData2 populateBean(String[] headers, CellProcessor[] processors, int rowNumber, int lineNumber, List<String> row) {
-        MeterData2 retVal = instantiateBean(MeterData2.class);
+    private MeterData2 populateBean(List<String> row) {
+        MeterData2 retVal = new MeterData2();
 
-        for (int i = 0; i < headers.length; i++) {
-            CsvContext context = new CsvContext(lineNumber, rowNumber, i);
-            Object fieldValue = processors[i].execute(row.get(i), context);
-
-            Method setMethod = cache.getSetMethod(retVal, headers[i], fieldValue.getClass());
-            invokeSetter(retVal, setMethod, fieldValue);
-        }
+        retVal.setSein(row.get(0));
+        retVal.setReadingDateTime(parseDateTime(row.get(1), row.get(2)));
+        retVal.setKwd(getNumericValue(row.get(3)));
+        retVal.setKwhd(getNumericValue(row.get(4)));
+        retVal.setKvarhd(getNumericValue(row.get(5)));
+        retVal.setKwr(getNumericValue(row.get(6)));
+        retVal.setKwhr(getNumericValue(row.get(7)));
+        retVal.setKvarhr(getNumericValue(row.get(8)));
+        retVal.setEstimationFlag(row.get(9));
 
         return retVal;
     }
 
-    private static void invokeSetter(final Object bean, final Method setMethod, final Object fieldValue) {
+    private Double getNumericValue(String data) {
+        if (NumberUtils.isParsable(data)) {
+            return Double.parseDouble(data);
+        } else {
+            return null;
+        }
+    }
+
+    private Date parseDateTime(String date, String time) {
+        Date retVal = null;
+
         try {
-            setMethod.invoke(bean, fieldValue);
-        }
-        catch(final Exception e) {
-            throw new SuperCsvReflectionException(String.format("error invoking method %s()", setMethod.getName()), e);
-        }
-    }
-
-    private <T> T instantiateBean(final Class<T> clazz) {
-        T bean;
-
-        try {
-            bean = clazz.newInstance();
-        } catch (InstantiationException e) {
-            throw new SuperCsvReflectionException(String.format(
-                    "error instantiating bean, check that %s has a default no-args constructor", clazz.getName()), e);
-        } catch (IllegalAccessException e) {
-            throw new SuperCsvReflectionException("error instantiating bean", e);
+            retVal = dateFormat.parse(date + " " + time);
+        } catch (ParseException e) {
+            log.error(e.getMessage(), e);
         }
 
-        return bean;
-    }
-
-    private String[] getHeaders() {
-        return new String[]{
-                "sein",
-                "readingDate",
-                "readingTime",
-                "kwd",
-                "kwhd",
-                "kvarhd",
-                "kwr",
-                "kwhr",
-                "kvarhr"
-        };
-    }
-
-    private CellProcessor[] getProcessors() {
-        return new CellProcessor[]{
-                new Trim(),
-                new Trim(),
-                new Trim(),
-                new ParseDouble(),
-                new ParseDouble(),
-                new ParseDouble(),
-                new ParseDouble(),
-                new ParseDouble(),
-                new ParseDouble()
-        };
+        return retVal;
     }
 
 }
---
+++ metering-app/src/test/java/com/pemc/crss/metering/parser/CSVReaderTest.java
@@ -0,0 +1,38 @@
+package com.pemc.crss.metering.parser;
+
+import com.pemc.crss.metering.dto.MeterData2;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+// TODO: Convert to spock
+public class CSVReaderTest {
+
+    @Test
+    public void parseCSVWithMissingColumns() throws IOException {
+        MeterQuantityReader reader = new CSVReader();
+        List<MeterData2> meterData = reader.readData(new FileInputStream(
+                new File(MDEFReaderTest2.class.getClassLoader().getResource(
+                        "meterdata/csv/MF3MABAMSUZ01.csv").getFile())));
+
+        assertThat(meterData.size(), is(equalTo(2976)));
+    }
+
+    @Test
+    public void parseCSVWithExtraColumns() throws IOException {
+        MeterQuantityReader reader = new CSVReader();
+        List<MeterData2> meterData = reader.readData(new FileInputStream(
+                new File(MDEFReaderTest2.class.getClassLoader().getResource(
+                        "meterdata/csv/MET_CEDCMSP_R3MEXCEDC01TNSC01_20161003.csv").getFile())));
+
+        assertThat(meterData.size(), is(equalTo(96)));
+    }
+
+}
