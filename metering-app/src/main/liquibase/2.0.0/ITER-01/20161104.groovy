2f221799b3abffe3f0c1c29a559dbfe7cff860aa
Julius Cessar Lapugot <jlapugot@exist.com>
2016-11-04 19:53:28 +0800

    Rename table for bcq file
     - rename also some columns for better readability

--- metering-app/src/main/liquibase/2.0.0/ITER-01/20161104.groovy
+++ metering-app/src/main/liquibase/2.0.0/ITER-01/20161104.groovy
@@ -5,14 +5,13 @@ FILE = "20161104"
 databaseChangeLog(logicalFilePath: "/liquibase/${REL}/${ITER}") {
 
     changeSet(id: "${REL}_${ITER}_${FILE}_01", author: 'jlapugot', failOnError: true) {
-        createTable(tableName: 'txn_bcq_manifest_file', remarks: 'BCQ manifest for each file') {
+        createTable(tableName: 'txn_bcq_upload_file', remarks: 'BCQ upload file') {
             column(name: 'file_id',         type: 'BIGINT')  { constraints(primaryKey: true, nullable: false) }
             column(name: 'transaction_id',  type: 'VARCHAR(36)') { constraints(nullable: false) }
             column(name: 'filename',        type: 'VARCHAR(50)')
             column(name: 'filesize',        type: 'BIGINT')
             column(name: 'status',          type: 'VARCHAR(13)', remarks:
-                    'Possible values: CONFIRMED, NOT_CONFIRMED, NULLIFIED, NOT_NULLIFIED, FOR_CONFIRMATION, FOR_NULLIFICATION')
-            column(name: 'error_details',   type: 'VARCHAR(250)', remarks: 'Contains any validation errors messages if there are any')
+                    'Possible values: CONFIRMED, NOT_CONFIRMED, NULLIFIED, NOT_NULLIFIED')
         }
 
         createTable(tableName: 'txn_bcq_data', remarks: 'BCQ data') {
@@ -20,7 +19,7 @@ databaseChangeLog(logicalFilePath: "/liquibase/${REL}/${ITER}") {
             column(name: 'file_id',                   type: 'BIGINT') { constraints(nullable: false) }
             column(name: 'selling_participant_name',  type: 'VARCHAR(255)') { constraints(nullable: false) }
             column(name: 'selling_mtn',               type: 'VARCHAR(255)') { constraints(nullable: false) }
-            column(name: 'buyer_id',                  type: 'BIGINT', remarks: 'Buying participant id') { constraints(nullable: false) }
+            column(name: 'buying_participant_id',     type: 'BIGINT') { constraints(nullable: false) }
             column(name: 'reference_mtn',             type: 'VARCHAR(255)') { constraints(nullable: false) }
             column(name: 'start_time',                type: 'TIMESTAMP') { constraints(nullable: false) }
             column(name: 'end_time',                  type: 'TIMESTAMP') { constraints(nullable: false) }
