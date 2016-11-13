67e3f48e9630f18c24c8bf99e353d263b2123e6a
Chris Lim <clim@exist.com>
2016-11-13 18:39:37 +0800

    Added missing class.

---
+++ metering-upload/src/main/java/com/pemc/crss/meter/upload/ParticipantName.java
@@ -0,0 +1,43 @@
+package com.pemc.crss.meter.upload;
+
+// TODO: Use lombok
+public class ParticipantName {
+
+    private long id;
+    private String shortName;
+    private String participantName;
+    private String registrationCategory;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getParticipantName() {
+        return participantName;
+    }
+
+    public void setParticipantName(String participantName) {
+        this.participantName = participantName;
+    }
+
+    public String getRegistrationCategory() {
+        return registrationCategory;
+    }
+
+    public void setRegistrationCategory(String registrationCategory) {
+        this.registrationCategory = registrationCategory;
+    }
+
+}
