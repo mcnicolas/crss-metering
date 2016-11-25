aef6e1335fc3900a4402874d0a2b9c2d11c023fa
Julius Cessar Lapugot <jlapugot@exist.com>
2016-11-25 09:18:24 +0800

    [BCQ] Add new dto for displaying of BCQ declarations

---
+++ metering-app/src/main/java/com/pemc/crss/metering/dto/BcqDeclarationDisplay.java
@@ -0,0 +1,18 @@
+package com.pemc.crss.metering.dto;
+
+import lombok.Data;
+
+@Data
+public class BcqDeclarationDisplay {
+
+    private String sellingParticipantName;
+    private String sellingParticipantShortName;
+    private String sellingMtn;
+    private String buyingParticipant;
+    private String tradingDate;
+    private String transactionID;
+    private String submittedDate;
+    private String updatedVia;
+    private String status;
+
+}
