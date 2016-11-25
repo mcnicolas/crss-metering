f989800c74506be5a6e65b11e58ad9c4d8e06d54
Chris Lim <clim@exist.com>
2016-11-25 16:26:28 +0800

    [140699] Fix for intermittent error when uploading meter quantity

--- metering-app/src/main/java/com/pemc/crss/metering/listener/MeterQuantityListener.java
+++ metering-app/src/main/java/com/pemc/crss/metering/listener/MeterQuantityListener.java
@@ -3,7 +3,6 @@ package com.pemc.crss.metering.listener;
 import com.pemc.crss.metering.service.MeterService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
-import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
@@ -11,8 +10,9 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.amqp.core.ExchangeTypes.DIRECT;
 
 @Slf4j
 @Component
@@ -25,13 +25,11 @@ public class MeterQuantityListener {
         this.meterService = meterService;
     }
 
-    @Async
-    @RabbitListener(bindings = {
-            @QueueBinding(
-                    value = @Queue(value = "meter.quantity", durable = "true"),
-                    exchange = @Exchange(type = ExchangeTypes.TOPIC, value = "meter.quantity"),
-                    key = "meter.quantity")
-    })
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "crss.meter.quantity", durable = "true"),
+            exchange = @Exchange(type = DIRECT, value = "crss.meter.quantity"),
+            key = "crss.meter.quantity")
+    )
     public void processMeterQuantityFile(@Header int headerID,
                                          @Header String transactionID,
                                          @Header String fileName,
@@ -41,6 +39,9 @@ public class MeterQuantityListener {
                                          @Header String mspShortName,
                                          @Header String category,
                                          @Payload byte[] fileContent) {
+
+        log.debug("Received MQ File. headerID:{} txnID:{} fileName:{} checksum:{} mspShortName:{} category:{}",
+                headerID, transactionID, fileName, checksum, mspShortName, category);
 
         try {
             long fileID = meterService.saveFileManifest(headerID, transactionID, fileName, fileType, fileSize, checksum);
