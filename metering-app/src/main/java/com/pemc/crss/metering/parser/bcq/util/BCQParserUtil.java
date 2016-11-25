f9349fa908bf7507444d2a3616bbcddebcd77b8c
Julius Cessar Lapugot <jlapugot@exist.com>
2016-11-25 11:33:03 +0800

    [BCQ] Implement strictly date parsing

--- metering-app/src/main/java/com/pemc/crss/metering/parser/bcq/util/BCQParserUtil.java
+++ metering-app/src/main/java/com/pemc/crss/metering/parser/bcq/util/BCQParserUtil.java
@@ -21,7 +21,7 @@ public class BCQParserUtil {
 
     public static Date parseDateTime(String dateString) {
         try {
-            return DateUtils.parseDate(dateString, DATE_TIME_FORMATS);
+            return DateUtils.parseDateStrictly(dateString, DATE_TIME_FORMATS);
         } catch (ParseException ignored) {}
 
         return null;
@@ -29,7 +29,7 @@ public class BCQParserUtil {
 
     public static Date parseDate(String dateString) {
         try {
-            return DateUtils.parseDate(dateString, DATE_TIME_FORMATS);
+            return DateUtils.parseDateStrictly(dateString, DATE_TIME_FORMATS);
         } catch (ParseException ignored) {}
 
         return null;
