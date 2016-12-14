3b9a056e3f597fd9a43a9e08cc673931d92ce0f1
Julius Cessar Lapugot <jlapugot@exist.com>
2016-12-14 19:49:08 +0800

    [BCQ] Add BCQ config keys enum

---
+++ metering-app/src/main/java/com/pemc/crss/metering/constants/ConfigKeys.java
@@ -0,0 +1,10 @@
+package com.pemc.crss.metering.constants;
+
+public enum ConfigKeys {
+
+    BCQ_DECLARATION_DEADLINE,
+    BCQ_NULLIFICATION_DEADLINE,
+    BCQ_UPDATE_DATELINE,
+    BCQ_INTERVAL
+
+}
