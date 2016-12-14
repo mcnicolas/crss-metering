e740c2c14bc3c6496e6633aa2eb13850cd6f6976
Julius Cessar Lapugot <jlapugot@exist.com>
2016-12-14 21:04:29 +0800

    [BCQ] Initial implementation for expired BCQs

--- metering-app/src/main/java/com/pemc/crss/metering/listener/BcqConfirmationListener.java
+++ metering-app/src/main/java/com/pemc/crss/metering/listener/BcqConfirmationListener.java
@@ -1,17 +1,31 @@
 package com.pemc.crss.metering.listener;
 
+import com.pemc.crss.metering.dao.BcqDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.pemc.crss.metering.constants.BcqStatus.FOR_CONFIRMATION;
+import static com.pemc.crss.metering.constants.BcqStatus.NOT_CONFIRMED;
 import static org.springframework.amqp.core.ExchangeTypes.DIRECT;
 
 @Slf4j
 @Component
 public class BcqConfirmationListener {
+
+    private final BcqDao bcqDao;
+
+    @Autowired
+    public BcqConfirmationListener(BcqDao bcqDao) {
+        this.bcqDao = bcqDao;
+    }
 
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = "crss.bcq.confirmation", durable = "true"),
@@ -19,6 +33,12 @@ public class BcqConfirmationListener {
             key = "crss.bcq.confirmation"))
     public void processConfirmation() {
         log.debug("Received BCQ confirmation expiration trigger.");
+
+        Map<String, String> params = new HashMap<>();
+        params.put("expired", "expired");
+        params.put("status", FOR_CONFIRMATION.toString());
+        bcqDao.findAllHeaders(params).forEach(header ->
+                bcqDao.updateHeaderStatus(header.getHeaderId(), NOT_CONFIRMED));
     }
 
 }
--- metering-app/src/main/java/com/pemc/crss/metering/listener/BcqNullificationListener.java
+++ metering-app/src/main/java/com/pemc/crss/metering/listener/BcqNullificationListener.java
@@ -1,17 +1,31 @@
 package com.pemc.crss.metering.listener;
 
+import com.pemc.crss.metering.dao.BcqDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.pemc.crss.metering.constants.BcqStatus.CONFIRMED;
+import static com.pemc.crss.metering.constants.BcqStatus.FOR_NULLIFICATION;
 import static org.springframework.amqp.core.ExchangeTypes.DIRECT;
 
 @Slf4j
 @Component
 public class BcqNullificationListener {
+
+    private final BcqDao bcqDao;
+
+    @Autowired
+    public BcqNullificationListener(BcqDao bcqDao) {
+        this.bcqDao = bcqDao;
+    }
 
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = "crss.bcq.nullification", durable = "true"),
@@ -19,6 +33,12 @@ public class BcqNullificationListener {
             key = "crss.bcq.nullification"))
     public void processNullification() {
         log.debug("Received BCQ nullification expiration trigger.");
+
+        Map<String, String> params = new HashMap<>();
+        params.put("expired", "expired");
+        params.put("status", FOR_NULLIFICATION.toString());
+        bcqDao.findAllHeaders(params).forEach(header ->
+                bcqDao.updateHeaderStatus(header.getHeaderId(), CONFIRMED));
     }
 
 }
