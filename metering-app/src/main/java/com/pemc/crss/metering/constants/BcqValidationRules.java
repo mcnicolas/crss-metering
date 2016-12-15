62e936328fc5cfe0bad5e382ae9192b1ee2773fe
Julius Cessar Lapugot <jlapugot@exist.com>
2016-12-15 14:15:34 +0800

    [BCQ] Format error messages, make field values bold

--- metering-app/src/main/java/com/pemc/crss/metering/constants/BcqValidationRules.java
+++ metering-app/src/main/java/com/pemc/crss/metering/constants/BcqValidationRules.java
@@ -4,14 +4,15 @@ public enum BcqValidationRules {
 
     EMPTY("No data found."),
     EMPTY_LINE("Empty line is not allowed."),
-    INCORRECT_DECLARED_INTERVAL("Incorrect declared interval. Interval %s is not valid."),
-    INCORRECT_COLUMN_HEADER_COUNT("Incorrect column header count. Number of columns should only be 5."),
-    INCORRECT_TIME_INTERVALS("Incorrect time interval. Value %s is not valid for interval %s"),
-    DUPLICATE_DATE("Duplicate date. Duplicate entry for %s found under Selling MTN %s and Buyer %s."),
-    INCOMPLETE_ENTRIES("Incomplete BCQ entries for date of %s. Selling MTN %s and Buyer %s should have %s entries."),
-    INCORRECT_DATA_TYPE("Incorrect data type. %s should be %s."),
-    INCORRECT_FORMAT("Incorrect data format. %s should follow the format %s."),
-    CLOSED_TRADING_DATE("Trading date is closed. Submission of BCQ is closed for %s."),
+    INCORRECT_DECLARED_INTERVAL("Incorrect declared interval. Interval <b>%s</b> is not valid."),
+    INCORRECT_COLUMN_HEADER_COUNT("Incorrect column header count. Number of columns should only be <b>5</b>."),
+    INCORRECT_TIME_INTERVALS("Incorrect time interval. Value <b>%s</b> is not valid for interval <b>%s</b>"),
+    DUPLICATE_DATE("Duplicate date. Duplicate entry for <b>%s</b> found under Selling MTN <b>%s</b> and Buyer <b>%s</b>."),
+    INCOMPLETE_ENTRIES("Incomplete BCQ entries for date of <b>%s</b>. " +
+            "Selling MTN <b>%s</b> and Buyer <b>%s</b> should have <b>%s</b> entries."),
+    INCORRECT_DATA_TYPE("Incorrect data type. %s should be <b>%s</b>."),
+    INCORRECT_FORMAT("Incorrect data format. <b>%s</b> should follow the format <b>%s</b>."),
+    CLOSED_TRADING_DATE("Trading date is closed. Submission of BCQ is closed for <b>%s</b>."),
     INVALID_TRADING_DATE("Trading date is invalid. Only one trading date can be accepted per file."),
     NEGATIVE_BCQ("BCQ is Negative. Declared BCQs should not be negative."),
     MISSING_SELLING_MTN("Missing selling MTN."),
@@ -20,7 +21,8 @@ public enum BcqValidationRules {
     MISSING_DATE("Missing date."),
     MISSING_BCQ("Missing BCQ."),
     INCOMPLETE_REDECLARATION_ENTRIES("Incomplete BCQ entries. " +
-            "Redeclaration for date %s should also have entries of the ff. Selling MTN and Buyer pair(s):\n %s");
+            "Redeclaration for date <b>%s</b> should also have entries of the ff. " +
+            "Selling MTN and Buyer pair(s):\n <b>%s</b>");
 
     private final String errorMessage;
 
