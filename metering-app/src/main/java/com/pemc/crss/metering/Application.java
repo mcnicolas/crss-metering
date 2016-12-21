93199cb28bf968fe132c1f2c9612b25385d6301b
Monte Cillo Co <mco@exist.com>
2016-12-21 21:48:51 +0800

    Enable username mapped diagnostic context

--- metering-app/src/main/java/com/pemc/crss/metering/Application.java
+++ metering-app/src/main/java/com/pemc/crss/metering/Application.java
@@ -1,9 +1,11 @@
 package com.pemc.crss.metering;
 
+import com.pemc.crss.commons.logger.MDCLoggingSupport;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -18,4 +20,8 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public MDCLoggingSupport MDCLoggingSupport() {
+        return new MDCLoggingSupport();
+    }
 }
