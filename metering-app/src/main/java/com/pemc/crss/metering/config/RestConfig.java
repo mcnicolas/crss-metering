7fc5618d35f926d8ba0db549f153adf978120cd4
Julius Cessar Lapugot <jlapugot@exist.com>
2016-12-22 17:57:23 +0800

    [BCQ] Refactor json converter

--- metering-app/src/main/java/com/pemc/crss/metering/config/RestConfig.java
+++ metering-app/src/main/java/com/pemc/crss/metering/config/RestConfig.java
@@ -1,20 +1,33 @@
 package com.pemc.crss.metering.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.UNWRAP_ROOT_VALUE;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRAP_ROOT_VALUE;
+
 @Configuration
 public class RestConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+        restTemplate.getMessageConverters().add(jsonConverter());
         return restTemplate;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.getObjectMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .configure(UNWRAP_ROOT_VALUE, false)
+                .configure(WRAP_ROOT_VALUE, false);
+        return converter;
     }
 
 }
