92d0a198593a3dd64f44171d7bba8691354ad7dd
Chris Lim <clim@exist.com>
2017-01-18 08:58:17 +0800

    Added missing file.

---
+++ metering-upload/src/main/java/com/pemc/crss/meter/upload/AuthorizationException.java
@@ -0,0 +1,9 @@
+package com.pemc.crss.meter.upload;
+
+public class AuthorizationException extends Exception {
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+
+}
