167565e47cf7047791b15213cec62a1082f5565d
Chris Lim <clim@exist.com>
2017-02-01 14:16:01 +0800

    [MQ] Additional field needed for Phase 2 query.

--- metering-app/src/main/liquibase/changelog-main.groovy
+++ metering-app/src/main/liquibase/changelog-main.groovy
@@ -1,4 +1,5 @@
 databaseChangeLog {
     includeAll(path: '2.0.0/ITER-10/', relativeToChangelogFile: 'true')
     includeAll(path: '2.0.0/ITER-12/', relativeToChangelogFile: 'true')
+    includeAll(path: '2.0.0/ITER-14/', relativeToChangelogFile: 'true')
 }
