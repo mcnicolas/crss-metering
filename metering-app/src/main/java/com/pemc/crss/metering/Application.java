0afe3b7fefd473948176a6527c31d3a4fcfd086a
Monte Cillo Co <mco@exist.com>
2017-02-08 11:02:56 +0800

    Remove service discovery support

--- metering-app/src/main/java/com/pemc/crss/metering/Application.java
+++ metering-app/src/main/java/com/pemc/crss/metering/Application.java
@@ -3,14 +3,12 @@ package com.pemc.crss.metering;
 import com.pemc.crss.commons.logger.MDCLoggingSupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@EnableDiscoveryClient
 @EnableAsync
 public class Application {
 
