532b131a590ec61460ff6e471df3bfbff9b9684b
Julius Cessar Lapugot <jlapugot@exist.com>
2017-03-07 10:10:17 +0800

    [BCQ-UT] Add unit test for billing id validation

--- metering-app/src/test/groovy/com/pemc/crss/metering/validator/bcq/helper/BillingIdValidationHelperTest.groovy
+++ metering-app/src/test/groovy/com/pemc/crss/metering/validator/bcq/helper/BillingIdValidationHelperTest.groovy
@@ -1,4 +1,35 @@
 package com.pemc.crss.metering.validator.bcq.helper
 
-class BillingIdValidationHelperTest {
+import com.pemc.crss.metering.dto.bcq.BillingIdShortNamePair
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static com.pemc.crss.metering.constants.ValidationStatus.ACCEPTED
+import static com.pemc.crss.metering.constants.ValidationStatus.REJECTED
+
+class BillingIdValidationHelperTest extends Specification {
+
+    def validationHelper = new BillingIdValidationHelper()
+
+    @Unroll
+    def 'validate billing id, validation status must be #status'() {
+        given:
+        def validBillingIds = [
+                new BillingIdShortNamePair(billingId: 'BILL1', tradingParticipantShortName: 'BUYER1'),
+                new BillingIdShortNamePair(billingId: 'BILL2', tradingParticipantShortName: 'BUYER2')
+        ]
+
+        when:
+        def result = validationHelper.validBillingIds(validBillingIds).test(billingIds);
+
+        then:
+        result.status == status
+
+        where:
+        billingIds                  || status
+        ['BILL1']                   || ACCEPTED
+        ['BILL1', 'BILL2']          || ACCEPTED
+        ['BILL1', 'BILL2', 'BILL3'] || REJECTED
+    }
+
 }
