c8db0702998f2085eb8bd8b86b7a3f2e4d3daeb7
Julius Cessar Lapugot <jlapugot@exist.com>
2017-03-07 10:46:51 +0800

    [BCQ-UT] Add unit test for overriding

--- metering-app/src/test/groovy/com/pemc/crss/metering/validator/bcq/helper/OverrideValidationHelperTest.groovy
+++ metering-app/src/test/groovy/com/pemc/crss/metering/validator/bcq/helper/OverrideValidationHelperTest.groovy
@@ -5,34 +5,34 @@ import com.pemc.crss.metering.service.BcqService
 import spock.lang.Specification
 import spock.lang.Unroll
 
-import static com.pemc.crss.metering.constants.BcqValidationError.INCOMPLETE_RESUBMISSION_ENTRIES
+import static com.pemc.crss.metering.constants.BcqValidationError.INCOMPLETE_OVERRIDE_ENTRIES
 import static com.pemc.crss.metering.constants.ValidationStatus.ACCEPTED
 import static com.pemc.crss.metering.constants.ValidationStatus.REJECTED
 import static com.pemc.crss.metering.utils.DateTimeUtils.now
 
-class ResubmissionValidationHelperTest extends Specification {
+class OverrideValidationHelperTest extends Specification {
 
     def bcqService = Mock(BcqService)
-    def validationHelper = new ResubmissionValidationHelper(bcqService)
+    def validationHelper = new OverrideValidationHelper(bcqService)
 
     @Unroll
     def 'validate resubmission, validation status must be #status'() {
         when:
-        def result = validationHelper.validResubmission(_ as String, now()).test([])
+        def result = validationHelper.validOverride(_ as String, now()).test([])
 
         then:
         1 * bcqService.findAllHeaders(_ as Map) >> currentHeaderList
         currentHeaderList.size() * bcqService.isHeaderInList(_ as BcqHeader, _ as List) >> isHeaderInList
         result.status == status
         if (result.errorMessage) {
-            result.errorMessage.validationError == INCOMPLETE_RESUBMISSION_ENTRIES
+            result.errorMessage.validationError == INCOMPLETE_OVERRIDE_ENTRIES
         }
 
         where:
 
         currentHeaderList << [
-                [new BcqHeader(headerId: 1)],
-                [new BcqHeader(headerId: 1), new BcqHeader(headerId: 2)]
+                [new BcqHeader(headerId: 1, tradingDate: now())],
+                [new BcqHeader(headerId: 1, tradingDate: now()), new BcqHeader(headerId: 2)]
         ]
 
         isHeaderInList || status
