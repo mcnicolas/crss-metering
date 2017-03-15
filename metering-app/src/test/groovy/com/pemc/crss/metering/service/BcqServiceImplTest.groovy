0f43d413f9da99f5d606c53525b593ad96e55a22
Julius Cessar Lapugot <jlapugot@exist.com>
2017-03-15 17:54:10 +0800

    [BCQ-UT] Add remaining unit tests for service

--- metering-app/src/test/groovy/com/pemc/crss/metering/service/BcqServiceImplTest.groovy
+++ metering-app/src/test/groovy/com/pemc/crss/metering/service/BcqServiceImplTest.groovy
@@ -3,9 +3,13 @@ package com.pemc.crss.metering.service
 import com.google.common.collect.ImmutableMap
 import com.pemc.crss.commons.cache.service.CacheConfigService
 import com.pemc.crss.commons.web.dto.datatable.PageableRequest
+import com.pemc.crss.metering.constants.BcqStatus
 import com.pemc.crss.metering.dao.BcqDao
 import com.pemc.crss.metering.dao.query.ComparisonOperator
 import com.pemc.crss.metering.dto.bcq.*
+import com.pemc.crss.metering.dto.bcq.specialevent.BcqEventValidationData
+import com.pemc.crss.metering.dto.bcq.specialevent.BcqSpecialEvent
+import com.pemc.crss.metering.dto.bcq.specialevent.BcqSpecialEventParticipant
 import com.pemc.crss.metering.service.exception.InvalidStateException
 import com.pemc.crss.metering.service.exception.OldRecordException
 import com.pemc.crss.metering.validator.bcq.BcqValidationResult
@@ -13,9 +17,7 @@ import org.springframework.data.domain.PageImpl
 import spock.lang.Specification
 import spock.lang.Unroll
 
-import static com.pemc.crss.metering.constants.BcqStatus.CANCELLED
-import static com.pemc.crss.metering.constants.BcqStatus.CONFIRMED
-import static com.pemc.crss.metering.constants.BcqStatus.NULLIFIED
+import static com.pemc.crss.metering.constants.BcqStatus.*
 import static com.pemc.crss.metering.constants.ValidationStatus.ACCEPTED
 import static com.pemc.crss.metering.constants.ValidationStatus.REJECTED
 import static com.pemc.crss.metering.dao.query.ComparisonOperator.IN
@@ -66,8 +68,11 @@ class BcqServiceImplTest extends Specification {
         }
 
         where:
-        isResubmission << [true, false, true, false]
-        status << [ACCEPTED, ACCEPTED, REJECTED, REJECTED]
+        isResubmission || status
+        true           || ACCEPTED
+        false          || ACCEPTED
+        true           || REJECTED
+        false          || REJECTED
     }
 
     @Unroll
@@ -113,8 +118,7 @@ class BcqServiceImplTest extends Specification {
                         sellingMtn: 'MTN2',
                         billingId: 'BILL2',
                         tradingDate: parseDate('12/12/2012')
-                )
-                ],
+                )],
                 [],
                 [],
                 []
@@ -225,13 +229,236 @@ class BcqServiceImplTest extends Specification {
                 new BcqHeader(headerId: 1L),
                 new BcqHeader(headerId: 1L, status: CANCELLED)
         ]
-        status << [CONFIRMED, NULLIFIED, CANCELLED]
+
         sameHeaders << [
                 [new BcqHeader(headerId: 2L)],
                 [],
                 [new BcqHeader(headerId: 1L)]
         ]
-        exception << [OldRecordException, InvalidStateException, InvalidStateException]
+
+        status    || exception
+        CONFIRMED || OldRecordException
+        NULLIFIED || InvalidStateException
+        CANCELLED || InvalidStateException
+    }
+
+    @Unroll
+    def "request for cancellation, with status #status"() {
+        when:
+        sut.requestForCancellation(1L)
+
+        then:
+        1 * bcqDao.findHeader(_ as Long) >> new BcqHeader(headerId: 1L, status: status)
+        1 * bcqDao.findSameHeaders(_ as BcqHeader, _ as List, _ as ComparisonOperator) >> sameHeaders
+        1 * bcqDao.updateHeaderStatusBySettlement(_ as Long, FOR_APPROVAL_CANCEL)
+        1 * bcqNotificationManager.sendSettlementUpdateStatusNotification(_ as BcqHeader)
+
+        where:
+        status << [CONFIRMED, SETTLEMENT_READY]
+        sameHeaders << [
+                [new BcqHeader(headerId: 1L)],
+                [new BcqHeader(headerId: 1L)]
+        ]
+    }
+
+    @Unroll
+    def "request for cancellation, with status #status, then throw exception"() {
+        when:
+        sut.requestForCancellation(1L)
+
+        then:
+        1 * bcqDao.findHeader(_ as Long) >> new BcqHeader(headerId: 1L, status: status)
+        1 * bcqDao.findSameHeaders(_ as BcqHeader, _ as List, _ as ComparisonOperator) >> sameHeaders
+        thrown(InvalidStateException)
+
+        where:
+        status << [FOR_APPROVAL_CANCEL, FOR_APPROVAL_NEW, FOR_APPROVAL_UPDATE]
+        sameHeaders << [
+                [new BcqHeader(headerId: 1L)],
+                [new BcqHeader(headerId: 1L)],
+                [new BcqHeader(headerId: 1L)]
+        ]
+    }
+
+    @Unroll
+    def "approve declaration, with status #status"() {
+        when:
+        sut.approve(1L)
+
+        then:
+        1 * bcqDao.findHeader(_ as Long) >> new BcqHeader(headerId: 1L, status: status)
+        if (status == FOR_APPROVAL_CANCEL) {
+            1 * bcqDao.updateHeaderStatus(_ as Long, _ as BcqStatus)
+            1 * bcqDao.findSameHeaders(_ as BcqHeader, _ as List, _ as ComparisonOperator) >> sameHeaders
+        } else {
+            2 * bcqDao.updateHeaderStatus(_ as Long, _ as BcqStatus)
+            2 * bcqDao.findSameHeaders(_ as BcqHeader, _ as List, _ as ComparisonOperator) >> sameHeaders
+        }
+
+        1 * bcqNotificationManager.sendApprovalNotification(_ as BcqHeader)
+
+        where:
+        status << [FOR_APPROVAL_CANCEL, FOR_APPROVAL_NEW, FOR_APPROVAL_UPDATE]
+        sameHeaders << [
+                [new BcqHeader(headerId: 1L)],
+                [new BcqHeader(headerId: 1L)],
+                [new BcqHeader(headerId: 1L)]
+        ]
+    }
+
+    @Unroll
+    def "approve declaration, with status #status, then throw exception"() {
+        when:
+        sut.approve(1L)
+
+        then:
+        1 * bcqDao.findHeader(_ as Long) >> new BcqHeader(headerId: 1L, status: status)
+        1 * bcqDao.findSameHeaders(_ as BcqHeader, _ as List, _ as ComparisonOperator) >> sameHeaders
+        thrown(exception)
+
+        where:
+        sameHeaders << [
+                [new BcqHeader(headerId: 1L)],
+                [new BcqHeader(headerId: 1L)],
+                [new BcqHeader(headerId: 2L)]
+        ]
+
+        status              || exception
+        CANCELLED           || InvalidStateException
+        SETTLEMENT_READY    || InvalidStateException
+        FOR_APPROVAL_CANCEL || OldRecordException
+    }
+
+    def "process unconfirmed headers"() {
+        given:
+        def tradingDate = now()
+        def unconfirmedHeaders = [
+                new BcqHeader(headerId: 1L, status: NOT_CONFIRMED, tradingDate: tradingDate,
+                        sellingParticipantShortName: 'GEN1', buyingParticipantShortName: 'PDU1'),
+                new BcqHeader(headerId: 2L, status: NOT_CONFIRMED, tradingDate: tradingDate,
+                        sellingParticipantShortName: 'GEN1', buyingParticipantShortName: 'PDU1'),
+        ]
+        when:
+        sut.processUnconfirmedHeaders()
+
+        then:
+        1 * bcqDao.findAllHeaders(_ as Map) >> unconfirmedHeaders
+        unconfirmedHeaders.size() * bcqDao.updateHeaderStatus(_ as Long, NOT_CONFIRMED)
+        1 * bcqNotificationManager.sendUnprocessedNotification(_ as List, NOT_CONFIRMED)
+    }
+
+    def "process unnullified headers"() {
+        given:
+        def tradingDate = now()
+        def unnullifiedHeaders = [
+                new BcqHeader(headerId: 1L, status: FOR_NULLIFICATION, tradingDate: tradingDate,
+                        sellingParticipantShortName: 'GEN1', buyingParticipantShortName: 'PDU1'),
+                new BcqHeader(headerId: 2L, status: FOR_NULLIFICATION, tradingDate: tradingDate,
+                        sellingParticipantShortName: 'GEN1', buyingParticipantShortName: 'PDU2'),
+        ]
+        when:
+        sut.processUnnullifiedHeaders()
+
+        then:
+        1 * bcqDao.findAllHeaders(_ as Map) >> unnullifiedHeaders
+        unnullifiedHeaders.size() * bcqDao.findSameHeaders(_ as BcqHeader, _ as List, _ as ComparisonOperator) >> []
+        unnullifiedHeaders.size() * bcqDao.updateHeaderStatus(_ as Long, CONFIRMED)
+        2 * bcqNotificationManager.sendUnprocessedNotification(_ as List, CONFIRMED)
+    }
+
+    def "process headers to settlement ready"() {
+        given:
+        def headerIdsToUpdate = [1L, 2L]
+
+        when:
+        sut.processHeadersToSettlementReady()
+
+        then:
+        1 * configService.getIntegerValueForKey(_ as String, _ as Integer) >> 2
+        1 * bcqDao.selectByStatusAndDeadlineDatePlusDays(_ as BcqStatus, _ as Integer) >> headerIdsToUpdate
+        headerIdsToUpdate.size() * bcqDao.updateHeaderStatus(_ as Long, SETTLEMENT_READY)
+    }
+
+    def "find all special events"() {
+        given:
+        def specialEvents = []
+        for (int i = 0; i < new Random().nextInt(100); i ++) {
+            specialEvents.add(new BcqSpecialEvent(eventId: i))
+        }
+
+        when:
+        def result = sut.findAllSpecialEvents()
+
+        then:
+        1 * bcqDao.findAllSpecialEvents() >> specialEvents
+        result.size() == specialEvents.size()
+    }
+
+    def "save special event"() {
+        given:
+        def specialEvent = new BcqSpecialEvent(
+                tradingParticipants: [
+                        new BcqSpecialEventParticipant(shortName: 'PDU1'),
+                        new BcqSpecialEventParticipant(shortName: 'PDU2')
+                ],
+                tradingDates: [
+                        parseDate('12/12/2012'),
+                        parseDate('12/13/2012')
+                ]
+        )
+
+        when:
+        def result = sut.saveSpecialEvent(specialEvent)
+
+        then:
+        1 * bcqDao.checkDuplicateParticipantTradingDates(['PDU1', 'PDU2'], specialEvent.getTradingDates()) >> []
+        1 * bcqDao.saveSpecialEvent(_ as BcqSpecialEvent) >> 1L
+        result == 1L
+    }
+
+    def "save special event, throw error"() {
+        given:
+        def specialEvent = new BcqSpecialEvent(
+                tradingParticipants: [
+                        new BcqSpecialEventParticipant(shortName: 'PDU1')
+                ],
+                tradingDates: [
+                        parseDate('12/12/2012'),
+                        parseDate('12/13/2012')
+                ]
+        )
+
+        when:
+        sut.saveSpecialEvent(specialEvent)
+
+        then:
+        1 * bcqDao.checkDuplicateParticipantTradingDates(['PDU1'], specialEvent.getTradingDates()) >> [BcqEventValidationData.newInstance()]
+        thrown(RuntimeException)
+    }
+
+    def "find event participants by trading date"() {
+        given:
+        def tradingDate = now()
+
+        when:
+        def result = sut.findEventParticipantsByTradingDate(tradingDate)
+
+        then:
+        1 * bcqDao.findEventParticipantsByTradingDate(tradingDate) >> [new BcqSpecialEventParticipant(shortName: 'PDU1')]
+        result.size() == 1
+        result[0].shortName == 'PDU1'
+    }
+    def "find event deadline date by trading date and participant"() {
+        given:
+        def shortName = 'PDU1'
+        def tradingDate = now()
+
+        when:
+        def result = sut.findEventDeadlineDateByTradingDateAndParticipant(tradingDate, shortName)
+
+        then:
+        1 * bcqDao.findEventDeadlineDateByTradingDateAndParticipant(tradingDate, shortName) >> parseDate('12/12/2012')
+        result == parseDate('12/12/2012')
     }
 
 }
