55d6f6c2f789934a15874e927eb94f5c0674ac6f
Julius Cessar Lapugot <jlapugot@exist.com>
2017-03-28 17:57:37 +0800

    [BCQ-UT] Removal of unused unit test class

--- metering-app/src/test/groovy/com/pemc/crss/metering/resource/BcqUnitTest.groovy
+++
@@ -1,7 +0,0 @@
-package com.pemc.crss.metering.resource
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication(scanBasePackages = 'com.crss.metering.resource')
-class BcqUnitTest {
-}
