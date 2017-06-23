aafb7ec49c60008c99fdee8b7074a98180b05140
Nas Villanueva <jvillanueva@exist.com>
2017-06-23 15:18:26 +0800

    [Devops] Fixed MQ Uploader user/auth endpoint

--- metering-upload/src/main/java/com/pemc/crss/meter/upload/EndPoint.java
+++ metering-upload/src/main/java/com/pemc/crss/meter/upload/EndPoint.java
@@ -2,8 +2,8 @@ package com.pemc.crss.meter.upload;
 
 public interface EndPoint {
 
-    String OAUTH_TOKEN = "/admin/oauth/token";
-    String USER_URL = "/admin/user";
+    String OAUTH_TOKEN = "/uaa/oauth/token";
+    String USER_URL = "/uaa/user";
     String PARTICIPANT_CATEGORY_URL = "/reg/participants/current/category";
     String MSP_LISTING_URL = "/reg/participants/category/msp";
     String UPLOAD_HEADER = "/metering/uploadHeader";
